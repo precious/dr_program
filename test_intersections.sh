96d3e9f407ddb55991b7fadea722107bbef03ddc
precious <precious@i.ua>
2012-03-27 22:24:09 +0300

    minor fixes

--- test_intersections.sh
+++ test_intersections.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-NUMBER_OF_TESTS=700
+NUMBER_OF_TESTS=100
 NUMBER_OF_INTERVALS=10
-NUMBER_OF_PARTICLES=30000
-RADIUS=136
-OUTPUT_FILE="${NUMBER_OF_TESTS}x${NUMBER_OF_PARTICLES}.result"
+NUMBER_OF_PARTICLES=3000000
+RADIUS=100
+NAME="${NUMBER_OF_TESTS}x${NUMBER_OF_PARTICLES}_(R=$RADIUS)"
+OUTPUT_FILE="$NAME.result"
 
 if [ ! -f "$OUTPUT_FILE" ] ; then
 	echo -n > $OUTPUT_FILE
@@ -16,7 +17,7 @@ if [ ! -f "$OUTPUT_FILE" ] ; then
 fi
 
 DATA_FILE="`./generate_gnuplot_data.py \"$OUTPUT_FILE\" $NUMBER_OF_INTERVALS`"
-IMAGE_FILE="${NUMBER_OF_TESTS}x${NUMBER_OF_PARTICLES}.png"
+IMAGE_FILE="$NAME.png"
 gnuplot -e "set term png; set output \"$IMAGE_FILE\"; plot \"$DATA_FILE\" using 1:2 with lines;"
 rm -rf "$DATA_FILE"
 xdg-open "$IMAGE_FILE"
