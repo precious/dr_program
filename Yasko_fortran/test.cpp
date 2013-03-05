ea0790e4babbda92709877ac6b1d168962a36152
Vsevolod <Vsevolod@zojax.com>
2013-03-05 19:23:17 +0200

    test program for fortran linking

--- Yasko_fortran/test.cpp
+++ Yasko_fortran/test.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 
+typedef double real;
+
 struct Vector3D {
-    double x,y,z;
-    Vector3D(double _x,double _y,double _z): x(_x), y(_y), z(_z) {};
+    real x,y,z;
+    Vector3D(real _x,real _y,real _z): x(_x), y(_y), z(_z) {};
 };
 
-extern "C" int laplace_(int,Vector3D*,Vector3D*,Vector3D*);
+extern "C" int laplace_(int*,Vector3D*,Vector3D*,Vector3D*);
 
-extern "C" int testfun_(int);
+//extern "C" int testfun_(int*);
 
 using namespace std;
 
 int main() {
-    Vector3D v1(1.,0.,0.), v2(0.,1.,0.), v3(0.,0.,1.);
-    
-    cout << testfun_(15) << "!" << endl;
-    
-    //cout << laplace_(10,&v1,&v2,&v3) << "!" << endl;
+    Vector3D v1(1.,2.,3.), v2(4.,5.,6.), v3(7.,8.,9.);
+
+    int p = 1;
+//    cout << testfun_(&p) << "!" << endl;
+
+    cout << laplace_(&p,&v1,&v2,&v3) << "!" << endl;
     //cout << f << "  " << d << endl;
-        
+
     return 0;
 }
